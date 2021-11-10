ff5b8f0afb2d2a0fcfdc819f063597238978f67d
Yoroshi Kune <yoroshi.kune@pm.me>
2021-11-10 17:50:56 +1030

    fix: add explicit height to scroll lock for more browser compatibility

--- src/mantine-hooks/src/use-scroll-lock/utils/get-lock-styles.ts
+++ src/mantine-hooks/src/use-scroll-lock/utils/get-lock-styles.ts
@@ -2,12 +2,15 @@ import { getScrollWidth } from './get-scroll-width';
 
 export const getLockStyles = ({ disableBodyPadding }) => {
   const scrollWidth = disableBodyPadding ? null : getScrollWidth();
+  const windowHeight = window.innerHeight;
 
   const styles = `body {
         --removed-scroll-width: ${scrollWidth}px;
+        --window-height: ${windowHeight}px;
         touch-action: none;
         overflow: hidden !important;
         position: relative !important;
+        height: var(--window-height) !important;
         ${scrollWidth ? 'padding-right: var(--removed-scroll-width) !important;' : ''}
         `;
 
